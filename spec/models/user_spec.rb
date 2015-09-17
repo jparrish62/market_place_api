c2b1cfba6850e711825a3bd0f60c319d4fef0bb1
Julian <parrishjulian62@gmail.com>
2015-09-17 04:40:10 -0400

    Generate authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User, type: :model do 
   before { @user = FactoryGirl.build(:user) }
-
+ 
   subject { @user }
   
   it { should respond_to(:email) }
@@ -10,4 +10,22 @@ RSpec.describe User, type: :model do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  it { should respond_to(:auth_token) }
+
+  it { should validate_uniqueness_of(:auth_token) }
+
+  describe "#generate_authentication_token!" do 
+  	it "generates a unique token" do 
+  		Devise.stub(:friendly_token).and_return("auniquetoken123")
+  		@user.generate_authentication_token!
+  		expect(@user.auth_token).to eq "auniquetoken123"
+  	end
+  end
+
+  it "generates another token when one already has been taken" do 
+  	existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+  	@user.generate_authentication_token!
+  	expect(@user.auth_token).not_to eq existing_user.auth_token
+  end
 end
